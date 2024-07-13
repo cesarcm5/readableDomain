b35142bcdf288d31b920e274a499082dda4e5f61
Cesar Contreras <cesar.alberto2003@outlook.com>
2024-07-13 02:31:40 +0000

    Second Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+pronoun.forEach((pronombre) => {
+  adj.forEach((adjetivo) => {
+    noun.forEach((sujeto) => {
+      console.log(pronombre + adjetivo + sujeto + ".com");
+    });
+  });
+});
+
