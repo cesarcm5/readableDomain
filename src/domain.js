fa600268596567af9f753a99f4c8870e7b87510c
Cesar Contreras <cesar.alberto2003@outlook.com>
2024-07-18 00:00:46 +0000

    Readability changes

--- src/domain.js
+++ src/domain.js
@@ -2,10 +2,14 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 
-pronoun.forEach((pronombre) => {
-  adj.forEach((adjetivo) => {
-    noun.forEach((sujeto) => {
-      console.log(pronombre + adjetivo + sujeto + ".com");
-    });
-  });
-});
+function generateDomain(pronombre, adjetivo, sujeto) {
+  return `${pronombre}${adjetivo}${sujeto}.com`;
+}
+
+const domains = pronoun.flatMap((pronombre) => 
+  adj.flatMap((adjetivo) => 
+    noun.map((sujeto) => generateDomain(pronombre, adjetivo, sujeto))
+  )
+);
+
+domains.forEach((domain) => console.log(domain));
